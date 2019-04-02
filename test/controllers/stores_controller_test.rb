3d508a2430b4346ca69a301fab66795f62e4e798
shkhuzae <shouqalkhuzaei@gmail.com>
2019-04-02 09:44:54 +0000

    tested store controller

--- test/controllers/stores_controller_test.rb
+++ test/controllers/stores_controller_test.rb
@@ -2,7 +2,11 @@ require 'test_helper'
 
 class StoresControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @store = stores(:one)
+    create_stores
+  end
+  
+  teardown do 
+    remove_stores
   end
 
   test "should get index" do
@@ -17,30 +21,30 @@ class StoresControllerTest < ActionDispatch::IntegrationTest
 
   test "should create store" do
     assert_difference('Store.count') do
-      post stores_url, params: { store: { active: @store.active, city: @store.city, latitude: @store.latitude, longitude: @store.longitude, name: @store.name, phone: @store.phone, state: @store.state, street: @store.street, zip: @store.zip } }
+      post stores_url, params: { store: { active: @cmu.active, city: @cmu.city, latitude: @cmu.latitude, longitude: @cmu.longitude, name: @cmu.name, phone: @cmu.phone, state: @cmu.state, street: @cmu.street, zip: @cmu.zip } }
     end
 
     assert_redirected_to store_url(Store.last)
   end
 
   test "should show store" do
-    get store_url(@store)
+    get store_url(@cmu)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_store_url(@store)
+    get edit_store_url(@cmu)
     assert_response :success
   end
 
   test "should update store" do
-    patch store_url(@store), params: { store: { active: @store.active, city: @store.city, latitude: @store.latitude, longitude: @store.longitude, name: @store.name, phone: @store.phone, state: @store.state, street: @store.street, zip: @store.zip } }
-    assert_redirected_to store_url(@store)
+    patch store_url(@cmu), params: { store: { active: @cmu.active, city: @cmu.city, latitude: @cmu.latitude, longitude: @cmu.longitude, name: @cmu.name, phone: @cmu.phone, state: @cmu.state, street: @cmu.street, zip: @cmu.zip } }
+    assert_redirected_to store_url(@cmu)
   end
 
   test "should destroy store" do
     assert_difference('Store.count', -1) do
-      delete store_url(@store)
+      delete store_url(@cmu)
     end
 
     assert_redirected_to stores_url
